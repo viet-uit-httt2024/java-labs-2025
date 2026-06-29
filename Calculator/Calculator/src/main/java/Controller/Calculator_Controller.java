8782c31110f6074e9d6023691b539f8cca47b17a
viet-uit-httt2024 <24522003@gm.uit.edu.vn>
2026-06-29 21:03:38 +0700

    write Calculator_Controller basically

--- Calculator/Calculator/src/main/java/Controller/Calculator_Controller.java
+++ Calculator/Calculator/src/main/java/Controller/Calculator_Controller.java
@@ -4,10 +4,60 @@
  */
 package Controller;
 
+import Model.Calculator_Model;
+import View.Calculator_GUI;
+import Model.Calculator_Model;
+
 /**
  *
  * @author ADMIN
  */
 public class Calculator_Controller {
+//Attribute
+
+    private Calculator_GUI view;
+    private Calculator_Model model;
+
+    private double currrentResult;
+    private String currentOperator = "";
+    private boolean firstInput = true;
+
+//Method
+    //Constructor
+    public Calculator_Controller(Calculator_GUI view, Calculator_Model model) {
+        this.view = view;
+        this.model = model;
+        addNumberEvents();
+        addOperatorEvents();
+        addEqualEvent();
+        addClearEvent();
+        addDotEvent();
+    }
+    //Việt
+    public void appendNumber(String number){
+        
+    }
+
+    public void addNumberEvents() {
+         view.getBt0().addActionListener(e -> appendNumber("0"));
+    }
+
+    public void addOperatorEvents() {
+
+    }
+
+    public void addEqualEvent() {
+
+    }
+
+    
+    //Thy
+    public void addClearEvent() {
+
+    }
+
+    public void addDotEvent() {
+
+    }
     
 }
